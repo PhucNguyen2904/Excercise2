68e1eaca0fff81e5a380907a8b3f2e09772f3907
PhucNguyen0429 <giaphucnguyen2904@gmail.com>
2024-08-27 20:44:25 +0700

    update

--- src/main/java/com/javaweb/repository/DistrictRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.repository;
-
-import com.javaweb.repository.Entity.DistrictEntity;
-
-import java.util.List;
-
-public interface DistrictRepository {
-    DistrictEntity findByID(Long id);
-}
--- src/main/java/com/javaweb/repository/RentAreaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.repository;
-
-import com.javaweb.repository.Entity.RentAreaEntity;
-
-import java.util.List;
-
-public interface RentAreaRepository {
-    List<RentAreaEntity> getValueByBuilding(Long buildingId);
-}
